6ba26079ee4adbeab589f722a5159b64388201a0
Astel <astelthottankara@gmail.com>
2021-04-17 17:05:37 +0530

    feat: add animated banner

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AuthenticatedToast from '../Auth/AuthenticatedToast/AuthenticatedToast';
+import Banner from './Banner/Banner';
 import './Home.css';
 
 function Home() {
@@ -8,11 +9,20 @@ function Home() {
 			<div className="container">
 				<div className="row">
 					<div className="banner_p1 col-lg-6">
-						<h1> <b>FARMER <span style={{ color: "#10B981" }}>TO</span> BUYER</b> </h1>
-						<h2><b>IN A SNAP</b></h2>
+						<h1>
+							{' '}
+							<b>
+								FARMER{' '}
+								<span style={{ color: '#10B981' }}>TO</span>{' '}
+								BUYER
+							</b>{' '}
+						</h1>
+						<h2>
+							<b>IN A SNAP</b>
+						</h2>
 					</div>
 					<div className="banner_p2 col-lg-6">
-						<img src="https://cdn2.vectorstock.com/i/thumb-large/79/86/farmer-and-crop-harvest-flat-composition-vector-20647986.jpg" />
+						<Banner />
 					</div>
 				</div>
 			</div>
