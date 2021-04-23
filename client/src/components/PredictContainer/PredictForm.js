d571f8ecec535e72d312583f847f081f0922edff
Pranav <prn2108@gmail.com>
2021-04-23 15:44:03 +0530

    feat: UI updated

--- client/src/components/PredictContainer/PredictForm.js
+++ client/src/components/PredictContainer/PredictForm.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { Form, Button, Card } from 'react-bootstrap';
+import './PredictForm.css';
 
 const PredictForm = () => {
 
@@ -49,47 +50,57 @@ const PredictForm = () => {
             <div className="container">
                 <div className="row">
                     <div style={{ margin: 'auto' }} className="my-5 col-lg-6 col-sm-6 col-md-6">
-                        <Card style={{ width: '20rem', margin: 'auto' }}>
+                        <Card style={{ width: '32rem', margin: 'auto' }}>
                             <Card.Body>
                                 <Form onSubmit={submitHandler}>
-                                    <Form.Group>
-                                        <Form.Label>Nitrogen (N)</Form.Label>
-                                        <Form.Control id="nitrogen" onChange={changeHandler} type="number" placeholder="Enter N amount" />
-                                    </Form.Group>
+                                    <div className="my-3 row">
+                                        <div className="col-lg-4 col-sm-12">
+                                            <Form.Group>
+                                                <Form.Label>Nitrogen (N)</Form.Label>
+                                                <Form.Control id="nitrogen" required onChange={changeHandler} type="number" placeholder="Enter N amount" />
+                                            </Form.Group>
+                                        </div>
+                                        <div className="col-lg-4 col-sm-12">
+                                            <Form.Group>
+                                                <Form.Label>Phosphorus (P)</Form.Label>
+                                                <Form.Control id="phosphorus" required onChange={changeHandler} type="number" placeholder="Enter P amount" />
+                                            </Form.Group>
+                                        </div>
+                                        <div className="col-lg-4 col-sm-12">
+                                            <Form.Group >
+                                                <Form.Label>Potassium (K)</Form.Label>
+                                                <Form.Control id="potassium" required onChange={changeHandler} type="number" placeholder="Enter K amount" />
+                                            </Form.Group>
+                                        </div>
+                                    </div>
 
-                                    <Form.Group>
-                                        <Form.Label>Phosphorus (P)</Form.Label>
-                                        <Form.Control id="phosphorus" onChange={changeHandler} type="number" placeholder="Enter P amount" />
-                                    </Form.Group>
-
-                                    <Form.Group >
-                                        <Form.Label>Potassium (K)</Form.Label>
-                                        <Form.Control id="potassium" onChange={changeHandler} type="number" placeholder="Enter K amount" />
-                                    </Form.Group>
-
-                                    <Form.Group>
-                                        <Form.Label>Temperature (T)</Form.Label>
-                                        <Form.Control id="temperature" onChange={changeHandler} type="number" step=".0001" placeholder="Enter T amount" />
-                                    </Form.Group>
-
-                                    <Form.Group>
-                                        <Form.Label>Humidity (H)</Form.Label>
-                                        <Form.Control id="humidity" onChange={changeHandler} type="number" step=".0001" placeholder="Enter H amount" />
-                                    </Form.Group>
-
-                                    <Form.Group>
-                                        <Form.Label>PH (ph)</Form.Label>
-                                        <Form.Control id="ph" onChange={changeHandler} type="number" step=".0001" placeholder="Enter ph amount" />
-                                    </Form.Group>
+                                    <div className="my-3 row">
+                                        <div className="col-lg-4 col-sm-12">
+                                            <Form.Group>
+                                                <Form.Label>Temperature (T)</Form.Label>
+                                                <Form.Control id="temperature" required onChange={changeHandler} type="number" step=".0001" placeholder="Enter T amount" />
+                                            </Form.Group>
+                                        </div>
+                                        <div className="col-lg-4 col-sm-12">
+                                            <Form.Group>
+                                                <Form.Label>Humidity (H)</Form.Label>
+                                                <Form.Control id="humidity" required onChange={changeHandler} type="number" step=".0001" placeholder="Enter H amount" />
+                                            </Form.Group>
+                                        </div>
+                                        <div className="col-lg-4 col-sm-12">
+                                            <Form.Group>
+                                                <Form.Label>PH (ph)</Form.Label>
+                                                <Form.Control id="ph" required onChange={changeHandler} type="number" step=".0001" placeholder="Enter ph amount" />
+                                            </Form.Group>
+                                        </div>
+                                    </div>
 
                                     <Form.Group>
                                         <Form.Label>Rainfall (R)</Form.Label>
-                                        <Form.Control id="rainfall" onChange={changeHandler} type="number" step=".0001" placeholder="Enter R amount" />
+                                        <Form.Control id="rainfall" required onChange={changeHandler} type="number" step=".0001" placeholder="Enter R amount" />
                                     </Form.Group>
 
-                                    <Button variant="primary" type="submit">
-                                        Submit
-                                   </Button>
+                                    <button type="submit" className="btn btn-light predict_button">Submit</button>
                                 </Form>
                             </Card.Body>
                         </Card>
@@ -98,8 +109,8 @@ const PredictForm = () => {
             </div>
             <div className="container">
                 <div className="row">
-                    <div style={{margin: 'auto'}} className="my-5 col-lg-6 col-md-6 col-sm-6">
-                        <Card style={{ width: '20rem', margin:'auto' }}>
+                    <div style={{ margin: 'auto' }} className="my-5 col-lg-6 col-md-6 col-sm-6">
+                        <Card style={{ width: '20rem', margin: 'auto' }}>
                             <Card.Body>
                                 <Card.Title>Optimal Crop is {crop}</Card.Title>
                             </Card.Body>
